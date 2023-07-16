c42ddedf05d699ec857b649b7051839deb04c3d1
Habibun Noby <habibun.noby@gmail.com>
2023-07-16 08:11:27 +0600

    feat: add match all

---
+++ src/Release/ES2020/matchAll.js
@@ -0,0 +1,15 @@
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
+
+// The matchAll() method returns an iterator of all results matching a string against a regular expression, including capturing groups.
+
+const regexp = /t(e)(st(\d?))/g;
+const str = 'test1test2';
+
+const array = [...str.matchAll(regexp)];
+
+console.log(array[0]);
+// Expected output: Array ["test1", "e", "st1", "1"]
+
+console.log(array[1]);
+// Expected output: Array ["test2", "e", "st2", "2"]
+
