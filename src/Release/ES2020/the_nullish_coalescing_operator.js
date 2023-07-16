c5c2acd0643eccbc3fe9fc7145dbb077aa5d5006
Habibun Noby <habibun.noby@gmail.com>
2023-07-16 08:45:19 +0600

    feat: add optional chaining operator

---
+++ src/Release/ES2020/the_nullish_coalescing_operator.js
@@ -0,0 +1,12 @@
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+
+// The nullish coalescing (??) operator is a logical operator that returns its right-hand side operand
+// when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+
+const foo = null ?? 'default string';
+console.log(foo);
+// Expected output: "default string"
+
+const baz = 0 ?? 42;
+console.log(baz);
+// Expected output: 0
